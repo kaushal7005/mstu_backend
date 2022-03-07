e74630700ff920cf6afd4e360f9f640b125f65bf
Kaushal Gajjar <kgajjar@integconsulting.com>
2022-03-07 10:04:11 +0530

    Add Lamda function

--- routes/singleEntry.js
+++ routes/singleEntry.js
@@ -187,15 +187,15 @@ router.route('/startEntry').post((req, res) => {
     const password = req.body.password; 
     let couponCode = "none"; 
     couponCode = req.body.couponCode;
-    let speed = 800;
+    let speed = 400;
     if(couponCode === "kkhome"){
-        speed=150;
+        speed=100;
     }
     if(couponCode === "freefun"){
-        speed=300;
+        speed=250;
     }
     if(couponCode === "sm"){
-        speed=200;
+        speed=175;
     }
     let today = new Date().toISOString().slice(0, 10);
     Entry.findOne({userId: username, eDate:today}, function(err,entry) {
@@ -218,150 +218,20 @@ router.route('/startEntry').post((req, res) => {
         //     res.json({"success":"Your task is completed"})
         // }    
     });
-    
-    axios.get('https://mdtpl.masterdigitaltechnology.com/Users/Login')
+    // User.findOne({userId: username,isFirst:"true"}, function(err, user) {
+
+    // });
+    axios.get(`https://shjhx4r8zj.execute-api.us-east-1.amazonaws.com/?userId=${username}&pass=${password}&speed=${speed}`)
     .then(async response => {
-        console.log("call login page");
-        let tokenKey = "__RequestVerificationToken";
-        const loginDom = new JSDOM(response.data);
-        let tokenValue = loginDom.window.document.getElementsByName(tokenKey)[0].value;
-            // const numberWrongData=user.wrong;
-            let remainData=0;
-            let totalData=0;
-            let wrongData=0;
-            let wrongIdList=[];
-            let captchaLength= 0;
-            let form=new FormData();
-            form.append('UserName', username);
-            form.append('Password', password);
-            form.append([tokenKey], tokenValue);
-            headers={
-                'Cookie':JSON.stringify(response.headers['set-cookie'])
-            }
-            await axios.post('https://mdtpl.masterdigitaltechnology.com/Users/Login', form, 
-            { headers: {...headers,...form.getHeaders()} })
-            .then(async response => {
-                console.log(`Login success in ${username}`);
-                let promises  = [];
-                await axios.get('https://mdtpl.masterdigitaltechnology.com/Users/JobWork',{ headers: {...headers, referer:"https://mdtpl.masterdigitaltechnology.com/Users/TodayWork"} })
-                .then(async response=>{
-                    const totalDataDom = new JSDOM(response.data);
-                    remainData = totalDataDom.window.document.getElementsByTagName("h3")[1].innerHTML;
-                    attemptedData = totalDataDom.window.document.getElementsByTagName("h3")[2].innerHTML
-                    totalData = totalDataDom.window.document.getElementsByTagName("h3")[0].innerHTML;
-                    wrongData = totalDataDom.window.document.getElementsByTagName("h3")[4].innerHTML;
-                    if(remainData != 0){
-                        captchaLength =  totalDataDom.window.document.getElementsByClassName("noselect")[0].innerHTML.length;
-                    }
-                    User.updateOne({userId:username},{ $set: { nCaptcha: totalData } })
-                    .then(() =>  console.log("Update total captcha successfully"))
-                    .catch(err => res.status(400).json('Error:'+ err + "total captcha not updated"));
-                    console.log(`Total Data is ${totalData}, Remain Data is ${remainData}, Wrong Data is ${wrongData}`);
-                })
-                .catch(error => {console.log(error);});
-                const error = randomNumber(1,100);
-                if (captchaLength == 12){
-                    for (let i = 1; i <= remainData; i++) {
-                        const number= randomNumber(100000000000, 999999999999)
-                        let captchaForm=new FormData();
-                        captchaForm.append('SystemCaptcha', wrongIdList.includes(i) ? randomNumber(100000000000, 999999999999) : number);
-                        captchaForm.append('EnteredCaptcha', number);
-                        captchaForm.append([tokenKey], tokenValue);
-                        promises.push(
-                            axios.post('https://mdtpl.masterdigitaltechnology.com/Users/JobWork', captchaForm, { headers: {...headers,...captchaForm.getHeaders(),referer:"https://mdtpl.masterdigitaltechnology.com/Users/TodayWork"} })
-                            .then(()=>{ 
-                                console.log(`Captcha = ${number} Loop = ${i}  ${wrongIdList.includes(i) ? "False" : "true" } Id= ${username}`);
-                            })
-                            .catch(error => {console.log(error);})
-                        )  
-                        // if (i%100 == 0){
-                        //     res.json({"entryComplete":i})  
-                        // }
-                        await new Promise(resolve => setTimeout(resolve,speed))
-                    }
-                }
-                if (captchaLength == 8){
-                    for (let i = 1; i <= remainData; i++) {
-                        const number= randomNumber(10000000, 99999999)
-                        let captchaForm=new FormData();
-                        captchaForm.append('SystemCaptcha', wrongIdList.includes(i) ? randomNumber(10000000, 99999999) : number);
-                        captchaForm.append('EnteredCaptcha', number);
-                        captchaForm.append([tokenKey], tokenValue);
-                        promises.push(
-                            axios.post('https://mdtpl.masterdigitaltechnology.com/Users/JobWork', captchaForm, { headers: {...headers,...captchaForm.getHeaders(),referer:"https://mdtpl.masterdigitaltechnology.com/Users/TodayWork"} })
-                            .then(()=>{ 
-                                console.log(`Captcha = ${number} Loop = ${i}  ${wrongIdList.includes(i) ? "False" : "true" } Id= ${username}`);
-                            })
-                            .catch(error => {console.log(error);})
-                        )
-                        // if (i%100 == 0){
-                        //     res.json({"entryComplete":i})  
-                        // }
-                        await new Promise(resolve => setTimeout(resolve,speed))
-                    }
-                }
-                if (captchaLength == 6){
-                    for (let i = 1; i <= remainData; i++) {
-                        const number= randomNumber(100000, 999999)
-                        let captchaForm=new FormData();
-                        captchaForm.append('SystemCaptcha', wrongIdList.includes(i) ? randomNumber(100000, 999999) : number);
-                        captchaForm.append('EnteredCaptcha', number);
-                        captchaForm.append([tokenKey], tokenValue);
-                        promises.push(
-                            axios.post('https://mdtpl.masterdigitaltechnology.com/Users/JobWork', captchaForm, { headers: {...headers,...captchaForm.getHeaders(),referer:"https://mdtpl.masterdigitaltechnology.com/Users/TodayWork"} })
-                            .then(()=>{ 
-                                console.log(`Captcha = ${number} Loop = ${i}  ${wrongIdList.includes(i) ? "False" : "true" } Id= ${username}`);
-                            })
-                            .catch(error => {console.log(error);})
-                        )
-                        // if (i%100 == 0){
-                        //     res.json({"entryComplete":i})  
-                        // }   
-                        await new Promise(resolve => setTimeout(resolve,speed))
-                    }
-                }
-                if (captchaLength == 4){
-                    for (let i = 1; i <= remainData; i++) {
-                        const number= randomNumber(1000, 9999)
-                        let captchaForm=new FormData();
-                        captchaForm.append('SystemCaptcha', wrongIdList.includes(i) ? randomNumber(1000, 9999) : number);
-                        captchaForm.append('EnteredCaptcha', number);
-                        captchaForm.append([tokenKey], tokenValue);
-                        promises.push(
-                            axios.post('https://mdtpl.masterdigitaltechnology.com/Users/JobWork', captchaForm, { headers: {...headers,...captchaForm.getHeaders(),referer:"https://mdtpl.masterdigitaltechnology.com/Users/TodayWork"} })
-                            .then(()=>{ 
-                                console.log(`Captcha = ${number} Loop = ${i}  ${wrongIdList.includes(i) ? "False" : "true" } Id= ${username}`);
-                            })
-                            .catch(error => {console.log(error);})
-                        )
-                        // if (i%100 == 0){
-                        //     res.json({"entryComplete":i})  
-                        // }  
-                        await new Promise(resolve => setTimeout(resolve,speed))
-                    }
-                }
-                return Promise.all(promises).then(() => {
-                    Entry.updateOne({userId:username , eDate:today},{ $set: { isEntryDone: true } })
-                    .then(() =>  console.log("set entry done in data base"))
-                    .catch(err => res.status(400).json('Error:'+ err)); 
-                    axios.get('https://mdtpl.masterdigitaltechnology.com/Users/Logout')
-                    .then(() => { 
-                        res.json({"success":"Your task is completed","isEntryDone":true})
-                        console.log("logout success");
-                    })  
-                    .catch(error => {
-                        res.json({"error":"some thing wrong"});
-                        console.log(error);
-                    });  
-                });            
-            })
-            .catch(error => {
-                console.log(error);
-            });     
+        console.log("response:- ",response.data); 
     })
     .catch(error => {
         console.log(error);
     });
+    setTimeout(()=>{ Entry.updateOne({userId:username , eDate:today},{ $set: { isEntryDone: true } })
+    .then(() =>  console.log("set entry done in data base"))
+    .catch(err => res.status(400).json('Error:'+ err));
+    res.json({"success":"Your task is completed","isEntryDone":true}); },300000);
 });
 
 // router.route('/singlePayment').post(async(req, res) => {
